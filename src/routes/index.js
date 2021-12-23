80a725b02e187255104d4b3942d44a46cbad79d2
tincy111 <tincymthomas1@gmail.com>
2021-12-23 13:04:52 +0530

    update routes file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Route,Switch} from 'react-router-dom';
 import Login from '../views/pages/authentication/Login/Login';
 import Signup from '../views/pages/authentication/Signup/Signup';
-import NotFound from '../pages/NotFound';
-import MockTest from '../pages/MockTest/MockTest';
+// import NotFound from '../pages/NotFound';
+// import MockTest from '../pages/MockTest/MockTest';
 import Dashboard from '../views/dashboard/Default';
 import MainLayout from '../layout/MainLayout';
 
@@ -15,7 +15,7 @@ const index = () => {
       <Route path="/login"><Login/></Route>
       <Route path="/signup"><Signup/></Route>
       <Route path="/dashboard"><MainLayout/></Route>
-      <Route path="*"><NotFound/></Route>
+      {/* <Route path="*"><NotFound/></Route> */}
     </Switch>
     </div> 
   )
